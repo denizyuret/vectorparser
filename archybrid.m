cfe255136a9b33a3e6af62c88b948ee6f185b320
Deniz Yuret <denizyuret@gmail.com>
2014-07-29 20:14:52 +0300

    Fixed bug in oracle_cost.

--- archybrid.m
+++ archybrid.m
@@ -61,31 +61,57 @@ end % switch
 end % transition
 
 
-% Oracle counts gold arcs that become impossible after moves:
-% 1. SHIFT moves n0 to s: (s\s0,n0) + (n0,s)
-% 2. RIGHT adds (s1,s0) : (s0,b) + (b,s0)
-% 3. LEFT  adds (n0,s0) : (s0,b) + (b\n0,s0) + (s1,s0)
+% Oracle counts gold arcs that become impossible after possible
+% transitions.  Tokens start their lifecycle in the buffer without
+% links.  They move to the top of the buffer (n0) with SHIFT moves.
+% There they acquire left dependents using LEFT moves.  After that a
+% single SHIFT moves them to the top of the stack (s0).  There they
+% acquire right dependents with SHIFT-RIGHT pairs.  Finally from s0
+% they acquire a head with a LEFT or RIGHT move.  Any token from the
+% buffer may become the head but only s1 from the stack may become a
+% left head.  The parser terminates with a single word at s0 whose
+% head is ROOT.
+%
+% 1. SHIFT moves n0 to s0: n0 cannot acquire left dependents after a
+% shift.  Also it can no longer get a head from the stack to the left
+% of s0 or get a root head if there is s0: (0+s\s0,n0) + (n0,s)
+%
+% 2. RIGHT adds (s1,s0): s0 cannot acquire a head or dependent from
+% the buffer after right: (s0,b) + (b,s0)
+%
+% 3. LEFT adds (n0,s0): s0 cannot acquire s1 or 0 (if there is no s1)
+% or ni (i>0) as head.  It also cannot acquire any more right
+% children: (s0,b) + (b\n0,s0) + (s1 or 0,s0)
 
 function c = oracle_cost(p, gold)
 assert(numel(gold) == p.nword);
 c = Inf(1, p.NMOVE);
 n0 = p.wptr;
-if (n0 <= p.nword)
-  c(p.SHIFT) = sum(gold(p.stack(1:p.sptr)) == n0) ...
-      + sum(p.stack(1:p.sptr-1) == gold(n0));
+
+if (n0 <= p.nword)      % shift is legal
+  n0h = gold(n0);
+  c(p.SHIFT) = sum(gold(p.stack(1:p.sptr)) == n0) + ...
+      + sum(p.stack(1:p.sptr-1) == n0h) ...
+      + ((n0h == 0) && (p.sptr >= 1));
 end
+
 if (p.sptr >= 1)
   s0 = p.stack(p.sptr);
   s0h = gold(s0);
   s0b = sum(gold(n0:end) == s0);
-  if (n0 <= p.nword)
-    c(p.LEFT) = s0b + (s0h > n0);
+
+  if (n0 <= p.nword)    % left is legal
+    c(p.LEFT) = s0b + ...
+        ((s0h > n0) || ...
+         ((p.sptr == 1) && (s0h == 0)) || ...
+         ((p.sptr >  1) && (s0h == p.stack(p.sptr-1))));
   end
-  if (p.sptr >= 2) 
+
+  if (p.sptr >= 2)      % right is legal
     c(p.RIGHT) = s0b + (s0h >= n0);
-    c(p.LEFT) = c(p.LEFT) + (s0h == p.stack(p.sptr-1));
   end
 end
+
 v = valid_moves(p);
 assert(all(isfinite(c(v))) && all(isinf(c(~v))),...
        '[%s] [%s]', num2str(v), num2str(c));
