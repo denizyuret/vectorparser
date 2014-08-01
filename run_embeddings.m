3cefbea12c641c74f94dbd674a3d63e8d7909ff4
Deniz Yuret <denizyuret@gmail.com>
2014-08-01 09:09:08 +0300

    Updated skip list.

--- run_embeddings.m
+++ run_embeddings.m
@@ -13,20 +13,75 @@ em = dir(root);
 for i=1:numel(em)
   emi = em(i).name;                     % e.g. conllToken_rcv1UNK
   if strcmp(emi(1),'.') continue; end
-  if any(strcmp(emi, {'conll08_wikipedia2MUNK-25', 
-                      'conllToken_cw25scaled', 
-                      'conllToken_rcv1UNK', 
-                      'conllToken_rnn80', 
-                      'conllToken_wikipedia2MUNK-25', 
-                      'conllToken_wikipedia2MUNK-50', 
-                      'conllWSJToken_cw25', 
-                      'conllWSJToken_cw25scaled', 
-                      'conllWSJToken_cw50scaled', 
-                      'conllWSJToken_hlbl50scaled', 
-                      'conllWSJToken_huang-0.1',
-                      'conllWSJToken_manaal48',
-                      'conllWSJToken_manaal48scaled01',
-                      'conllWSJToken_hpca50scaled01'
+  if any(strcmp(emi, {
+'conllWSJToken_bansal100',
+'conllWSJToken_wikipedia2MUNK-200',
+'conllWSJToken_wikipedia2MUNK-100',
+'conllToken_wikipedia2MUNK-50',
+'conllToken_wikipedia2MUNK-25',
+'conll_bansal100',
+'conllWSJToken_rcv1UNK50',
+'conllToken_rcv1UNK',
+'conllWSJToken_rcv1UNK',
+'conllWSJToken_rcv1UNK25',
+'conllWSJToken_dep-100',
+'conll_cw100scaled',
+'conllWSJToken_huang-0.1',
+'conll_4scode+f50',
+'conll_2scode+f100',
+'conll_3scode+f100',
+'conll_6scode+f50',
+'conll_4scode+f100',
+'conll_6scode+f100',
+'conll_5scode+f100',
+'conllWSJToken_dep-WSJ',
+'conll_10scode+f50',
+'conll_7scode+f50',
+'conll_2scode+f50',
+'conll_1scode+f100',
+'conll_1scode+f50',
+'conll_9scode+f50',
+'conll_3scode+f50',
+'conll_5scode+f50',
+'conll_8scode+f50',
+'conll_3scode+f25',
+'conllWSJToken_cw50scaled',
+'conll_cw50scaled',
+'conllWSJToken_ungar2-0.1',
+'conllWSJToken_hlbl50scaled',
+'conll_cw',
+'conllWSJToken_mikolovWiki-0.1',
+'conll_dep-100',
+'conll_dep-WSJ',
+'conll_cw25+cw25',
+'conll_cw25',
+'conllWSJToken_cw25',
+'conllWSJToken_stratos50k5000scaled01',
+'conllWSJToken_gsng1M',
+'conllWSJToken_mikolovRCV50scaled01',
+'conllWSJToken_cw25scaled',
+'conll_cw25scaled',
+'conllWSJToken_gsngAE',
+'conllWSJToken_gsngALL',
+'conll_dep100+wiki25',
+'conllWSJToken_yogamata52scaled01',
+'conllWSJToken_yogamata52',
+'conllWSJToken_stratos50k200scaled01',
+'conll_german+german',
+'conllWSJToken_manaal48',
+'conllWSJToken_hpca50scaled01',
+'conllWSJToken_manaal48scaled01',
+'conll_german',
+'conllWSJToken_rnn80',
+'conll_dep100+mik',
+'conllWSJToken_levy300',
+'conllWSJToken_mikolovGoogleNews300',
+'conllWSJToken_murphy50',
+'conllWSJToken_murphy50scaled01',
+'conllWSJToken_wikipedia2MUNK-25',
+'conll_10scode+f50',
+'conll_german2',
+
                      }))
     fprintf('Skipping %s\n', emi);
     continue; 
@@ -36,7 +91,7 @@ for i=1:numel(em)
   tic;
   trn_i = {};
   dev_i = {};
-  if isdir([root '/' emi '/24'])
+  if isdir([root '/' emi '/22'])
     for j=2:21                          % read train
       dir_j = sprintf('%s/%s/%02d', root, emi, j); % e.g. embedded/conllToken_rcv1UNK/02
       fprintf('Reading %s\n', dir_j);
@@ -60,7 +115,7 @@ for i=1:numel(em)
       dev_i = [dev_i dev_k];
     end % for k=1:numel(dp)
 
-  elseif isdir([root '/' emi '/02'])
+  elseif isdir([root '/' emi '/01'])
     dir_j = sprintf('%s/%s/%02d', root, emi, 0);
     fprintf('Reading %s\n', dir_j);
     dp = dir(dir_j);
