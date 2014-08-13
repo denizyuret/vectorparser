6c362bcf539c7a958ee1f654f7a88c9b1a285195
Deniz Yuret <denizyuret@gmail.com>
2014-08-13 08:37:56 +0300

    Profiling improvements.

--- beamparser.m
+++ beamparser.m
@@ -1,3 +1,5 @@
+function [model, dump] = beamparser_dbg(model, corpus, varargin)
+
 % Usage: beamparser takes a model, a corpus, and some options, and
 % outputs a model (untouched if update==0), and optionally a dump.
 % The dump can be given to eval_conll with the corpus to get some
@@ -8,64 +10,39 @@
 % [model,dump] = beamparser(model, corpus, 'update', 0)); %% testing
 % stats = eval_conll(corpus, dump);
 
-function [model, dump] = beamparser(model, corpus, varargin)
-
 m = beamparser_init(varargin, nargout);
+agenda = cell(m.beam * m.nmove, 2);
 fprintf('Processing sentences...\n');
 
 for snum=1:numel(corpus)
   sentence = corpus{snum};
-  % candidates are triples of score, state, history
-  s0 = 0;
-  p0 = feval(model.parser, numel(sentence.head));
-  h0 = struct('x',[],'y',[],'z',[],'score',[],'cost',[]);
-  candidates = { s0, p0, h0 };
-  agenda = {};
+  parser = feval(m.parser, numel(sentence.head));
+  candidates = {0, struct('parser', parser)};
   while 1
+    nagenda = 0;
     for cnum = 1:size(candidates, 1)
-      s0 = candidates{cnum,1};
-      p0 = candidates{cnum,2};
-      h0 = candidates{cnum,3};
-      if m.dbg fprintf('cand[%d]: %g %s\n', cnum, s0, mat2str(h0.z)); end
-      valid = p0.valid_moves;
-      if (sum(valid) == 0) continue; end
-      feats = features(p0, sentence, model.feats);
-      score = compute_scores(m, feats');
-      cost = p0.oracle_cost(sentence.head);
-      [~, mincostmove] = min(cost);
-      for move_i = 1:p0.NMOVE
-        if ~valid(move_i) continue; end
-        s1 = s0 + score(move_i);
-        p1 = copy(p0);
-        p1.transition(move_i);
-        h1 = h0;
-        h1.score = [h1.score, score'];  % score is a row vector
-        h1.cost = [h1.cost, cost'];     % cost is a row vector
-        h1.x = [h1.x, feats'];          % feats is a row vector
-        h1.y = [h1.y, mincostmove];
-        h1.z = [h1.z, move_i];
-        agenda = [ agenda; { s1, p1, h1 } ];
-        if m.dbg fprintf('agenda += %g %s\n', s1, mat2str(h1.z)); end
-      end
-    end
-    if (numel(agenda) == 0)  break; end
-    candidates = top_b(agenda, m.beam);
-    agenda = {};
+      [score, state] = candidates{cnum,:};
+      parser = state.parser;
+      valid = parser.valid_moves();
+      if (sum(valid) == 0) continue; end;
+      state.feats = features(parser, sentence, m.feats)';
+      state.score = compute_scores(m, state.feats)';
+      state.cost  = parser.oracle_cost(sentence.head);
+      candidates{cnum,2} = state;
+      for move = 1:m.nmove
+        if ~valid(move) continue; end;
+        newscore = score + state.score(move);
+        newstate = struct('parser', copy(parser), 'prev', state);
+        newstate.parser.transition(move);
+        newstate.move = move;
+        nagenda = nagenda + 1;
+        agenda(nagenda,:) = { newscore, newstate };
+      end % for move = 1:m.nmove
+    end % for cnum = 1:size(candidates, 1)
+    if (nagenda == 0)  break; end;
+    candidates = top_b(agenda(1:nagenda,:), m.beam);
   end % while 1
-
-  c1 = top_b(candidates, 1);
-  s1 = c1{1,1};
-  p1 = c1{1,2};
-  h1 = c1{1,3};
-  dump.pred{end+1} = p1.head;
-  dump.x = [dump.x, h1.x];
-  dump.y = [dump.y, h1.y];
-  dump.z = [dump.z, h1.z];
-  dump.s1 = [dump.s1, s1];
-  dump.score = [dump.score, h1.score];
-  dump.cost = [dump.cost, h1.cost];
-  dump.sidx = [dump.sidx, numel(dump.z)];
-
+  update_dump(top_b(candidates, 1));
   fprintf('.');
 end % for snum=1:numel(corpus)
 fprintf('\n');
@@ -73,82 +50,83 @@ fprintf('\n');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function opts = beamparser_init(varargin_save, nargout_save)
+function m = beamparser_init(varargin_save, nargout_save)
 
-opts = struct();      % opts is a struct of options.
+m = model;  % copy of model with extra information
 
 for vi = 1:2:numel(varargin_save)
   v = varargin_save{vi};
   v1 = varargin_save{vi+1};
   switch v
    case 'predict' 
-    opts.predict = v1;
+    m.predict = v1;
    case 'update'  
-    opts.update  = v1;
+    m.update  = v1;
    case 'average' 
-    opts.average = v1;
+    m.average = v1;
    case 'gpu'     
-    opts.gpu     = v1;
+    m.gpu     = v1;
    case 'beam'
-    opts.beam	 = v1;
+    m.beam	 = v1;
    case 'dbg'
-    opts.dbg	 = v1;
+    m.dbg	 = v1;
    otherwise 
     error('Usage: [model, dump] = beamparser(model, corpus, opts)');
   end
 end
 
-if ~isfield(opts, 'dbg')
-  opts.dbg = 0;
+if ~isfield(m, 'dbg')
+  m.dbg = 0;
 end
 
-if ~isfield(opts, 'beam')
-  opts.beam = 64;
+if ~isfield(m, 'beam')
+  m.beam = 64;
 end
 
-if ~isfield(opts, 'predict')
-  opts.predict = true;  % predict: use the model for prediction (default), otherwise follow gold moves
+if ~isfield(m, 'predict')
+  m.predict = true;  % predict: use the model for prediction (default), otherwise follow gold moves
 end
  
-if ~isfield(opts, 'update')
-  opts.update = true;   % update: train the model (default), otherwise model is not updated
+if ~isfield(m, 'update')
+  m.update = true;   % update: train the model (default), otherwise model is not updated
 end
 
-opts.dump = (nargout_save >= 2);
-opts.compute_costs = opts.update || opts.dump || ~opts.predict;
-opts.compute_features = opts.update || opts.dump || opts.predict;
-opts.compute_scores  = opts.update || opts.predict;
+m.dump = (nargout_save >= 2);
+m.compute_costs = m.update || m.dump || ~m.predict;
+m.compute_features = m.update || m.dump || m.predict;
+m.compute_scores  = m.update || m.predict;
 
 assert(isfield(model,'parser'), 'Please specify model.parser.');
 tmp_s = corpus{1};
 tmp_p = feval(model.parser, numel(tmp_s.head));
 nc = tmp_p.NMOVE;
+m.nmove = nc;
 
-if opts.compute_features
+if m.compute_features
   assert(isfield(model,'feats'), 'Please specify model.feats.');
   assert(size(model.feats, 2) == 3, 'The feats matrix needs 3 columns.');
   tmp_f = features(tmp_p, tmp_s, model.feats);
   nd = numel(tmp_f);
 end
 
-if opts.compute_scores
+if m.compute_scores
   assert(isfield(model,'kerparam') && ...
          strcmp(model.kerparam.type,'poly'), ...
          'Please specify poly kernel in model.kerparam.\n');
-  opts.kerparam = model.kerparam;
+  m.kerparam = model.kerparam;
 
-  if ~isfield(opts,'average')
-    opts.average = (isfield(model,'beta2') && ~isempty(model.beta2) && ~opts.update);
-  elseif opts.average
+  if ~isfield(m,'average')
+    m.average = (isfield(model,'beta2') && ~isempty(model.beta2) && ~m.update);
+  elseif m.average
     assert(isfield(model,'beta2') && ~isempty(model.beta2),...
            'Please set model.beta2 for averaged model.');
   end
 
   if ~isfield(model,'SV') || isempty(model.SV)
-    if opts.update
-      opts.svtr = zeros(0, nd);
-      opts.betatr = zeros(0, nc);
-      opts.betatr2 = zeros(0, nc);
+    if m.update
+      m.svtr = zeros(0, nd);
+      m.betatr = zeros(0, nc);
+      m.betatr2 = zeros(0, nc);
     else
       error('Please specify model.SV');
     end
@@ -157,75 +135,86 @@ if opts.compute_scores
     assert(size(model.beta, 1) == nc);
     assert(size(model.SV, 2) == size(model.beta, 2));
     assert(all(size(model.beta) == size(model.beta2)));
-    opts.svtr = model.SV';
-    opts.betatr = model.beta';
-    opts.betatr2 = model.beta2';
+    m.svtr = model.SV';
+    m.betatr = model.beta';
+    m.betatr2 = model.beta2';
   end
 
-  if ~isfield(opts, 'gpu')
-    opts.gpu = gpuDeviceCount(); % Use the gpu if there is one
+  if ~isfield(m, 'gpu')
+    m.gpu = gpuDeviceCount(); % Use the gpu if there is one
   end
 
-  if opts.gpu
+  if m.gpu
     assert(gpuDeviceCount()>0, 'No GPU detected.');
     fprintf('Loading model on GPU.\n');
     gpuDevice(1);
-    opts.svtr = gpuArray(opts.svtr);
-    opts.betatr = gpuArray(opts.betatr);
-    opts.betatr2 = gpuArray(opts.betatr2);
+    m.svtr = gpuArray(m.svtr);
+    m.betatr = gpuArray(m.betatr);
+    m.betatr2 = gpuArray(m.betatr2);
   end
 
-end % if opts.compute_scores
+end % if m.compute_scores
 
-if opts.predict
+if m.predict
   fprintf('Using predicted moves.\n');
 else
   fprintf('Using gold moves.\n');
-end % if opts.predict
+end % if m.predict
 
-if opts.dump
+if m.dump
   fprintf('Dumping results.\n');
-  if opts.compute_features
+  if m.compute_features
     dump.x = [];
     dump.sidx = [];
   end
-  if opts.compute_costs
+  if m.compute_costs
     dump.y = [];
     dump.cost = [];
   end
-  if opts.compute_scores
+  if m.compute_scores
     dump.z = [];
     dump.score = [];
     dump.s1 = [];
   end
-  if opts.predict
+  if m.predict
     dump.pred = {};
   end
-  if opts.compute_features
+  if m.compute_features
     dump.feats = model.feats;
     sentence = corpus{1};
     p0 = feval(model.parser, numel(sentence.head));
     [~,dump.fidx] = features(p0, sentence, model.feats);
   end
-end % if opts.dump
+end % if m.dump
 
 end % beamparser_init
 
 
 %%%%%%%%%%%%%%%%%%%%%%
-% function update_dump()
-% if opts.compute_features
-%   dump.x(:,end+1) = ftr;
-% end
-% if opts.compute_costs
-%   dump.y(end+1) = mincostmove;
-%   dump.cost(:,end+1) = cost;
-% end
-% if opts.compute_scores
-%   dump.z(end+1) = execmove;
-%   dump.score(:,end+1) = score;
-% end
-% end % update_dump
+function update_dump(c)
+[score, state] = c{:};
+dump.s1(end+1) = score;
+heads = state.parser.head;
+dump.pred{end+1} = heads;
+nstates = 2*numel(heads) - 1;
+states = cell(1, nstates);
+for i=nstates:-1:1
+  states{i} = state;
+  if i>1 
+    if ~isfield(state,'prev') error('Bad state'); end;
+    state = state.prev; 
+  end
+end
+for i=1:numel(states)-1
+  state = states{i};
+  dump.x(:,end+1) = state.feats;
+  dump.cost(:,end+1) = state.cost;
+  dump.score(:,end+1) = state.score;
+  dump.z(end+1) = states{i+1}.move;
+  [~,dump.y(end+1)] = min(state.cost);
+end
+dump.sidx(end+1) = numel(dump.z);
+end % update_dump
 
 end % beamparser
 
@@ -239,6 +228,10 @@ end % beamparser
 % score = gather(b + sum(bsxfun(@times, beta, (hp.gamma * (f * sv) + hp.coef0).^hp.degree),1)); % 11364us
 
 function score = compute_scores(m, ftr)
+if 0 %DBG
+  score = rand(1, size(m.betatr, 2));
+  return;
+end
 hp = m.kerparam;
 if isempty(m.svtr)
   score = zeros(1, size(m.betatr, 2));
