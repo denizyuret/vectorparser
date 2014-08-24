f2684ff1b77dbad01e19fd3cc7156302c6e32469
Deniz Yuret <denizyuret@gmail.com>
2014-08-24 13:46:15 +0300

    Cleanup.

--- compute_kernel.m
+++ compute_kernel.m
@@ -27,86 +27,89 @@ function scores = compute_kernel(m, x)
 % if m.average = 1, m.beta2 (averaged parameters) will be used
 % instead of m.beta.
 %
-% if m.cache is defined and not empty it will be used for lookup.
+% if m.cache is not empty it will be used for lookup.
 %
 % Implementation notes:
 % - Handle multiple sv blocks outside.  Do it using two models and adding results.
 % - Handle multiple x blocks and memory checking in here.
 
-  maxram = 32e9;          % max ram to use (32GB) if no gpu
-  nd = size(x, 1);        % dimensionality of instances / support vectors
-  nx = size(x, 2);        % number of instances
   nc = size(m.beta, 1);   % number of classes
-  ns = size(m.beta, 2);   % number of support vectors
-  scores = zeros(nc, nx);
-  todo = true(1, nx);
+  nx = size(x, 2);        % number of instances
+  todo = [];
 
-  if checkfield(m, 'cache')
-    for i=1:size(x, 2)
+  if ~isempty(m.cache)
+    scores = zeros(nc, nx);
+    todo = true(1, nx);
+    for i=1:nx
       s = m.cache.get(x);
       if ~isempty(s)
         scores(:,i) = s;
         todo(i) = false;
       end
     end
-    xtodo = x(:,todo);
-    nxtodo = sum(todo);
-  else
-    xtodo = x;
-    nxtodo = nx;
+    x = x(:,todo);
+    nx = sum(todo);
   end
 
-  if nxtodo == 0
+  if nx == 0
     % either nothing to do or all was cache hit
     return
   end
 
   hp = m.kerparam;
-  if checkfield(m, 'average')
+  if m.average
     beta = m.beta2;
   else
     beta = m.beta;
   end
 
-  % See how much memory we can use
-  if checkfield(m, 'gpu')
-    gdev = gpuDevice();
-    maxnumel = gdev.FreeMemory / 8;	% use all gpu memory
+  if nx == 1
+    batchsize = 1;
   else
-    gdev = [];
-    maxnumel = maxram / 8; 		% use 32GB memory
+    batchsize = maxbatchsize(m);
   end
 
-  % Figure out the maximum batch size nk
-  nk = floor(maxnumel / (2*ns + nd) - 20);
-  if (nk < 1) error('compute_kernel: Out of memory'); end;
-
-  fxtodo = zeros(nc, nxtodo);
-  for i=1:nk:nxtodo
-    j = min(nxtodo, i+nk-1);
-    xij = xtodo(:,i:j);
-    if ~isempty(gdev)
-      xij = gpuArray(xij);
-    end
+  fx = zeros(nc, nx);
+  for i=1:batchsize:nx
+    j = min(nx, i+batchsize-1);
+    xij = x(:,i:j);
     if strcmp(hp.type, 'rbf')
       x2 = sum(xij .^ 2, 1);
       s2 = sum(m.svtr .^ 2, 2);
-      fxtodo(:,i:j) = gather(beta * exp(-hp.gamma * bsxfun(@plus, x2, bsxfun(@plus, s2, -2 * (m.svtr * xij)))));
+      fx(:,i:j) = gather(beta * exp(-hp.gamma * bsxfun(@plus, x2, bsxfun(@plus, s2, -2 * (m.svtr * xij)))));
       clear x2 s2;
     elseif strcmp(hp.type, 'poly')
       if size(xij, 2) == 1
         % This hack roughly doubles speed on gpu if x is a single vector, sorry:
-        fxtodo(:,i:j) = gather(sum(bsxfun(@times, beta', (hp.gamma * (m.svtr * xij) + hp.coef0).^hp.degree),1))';
+        fx(:,i:j) = gather(sum(bsxfun(@times, beta', (hp.gamma * (m.svtr * xij) + hp.coef0).^hp.degree),1))';
       else
-        fxtodo(:,i:j) = gather(beta * (hp.gamma * (m.svtr * xij) + hp.coef0) .^ hp.degree);
+        fx(:,i:j) = gather(beta * (hp.gamma * (m.svtr * xij) + hp.coef0) .^ hp.degree);
       end
     else
       error('Unknown kernel type %s', hp.type);
     end  % if strcmp(hp.type, xxx)
-    clear xij;
-  end  % for i=1:nk:nx
-  scores(:,todo) = fxtodo;
+  end  % for i=1:batchsize:nx
+
+  if isempty(todo)
+    scores = fx;
+  else
+    scores(:,todo) = fx;
+  end
 
 end  % compute_kernel
 
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function batchsize = maxbatchsize(m)
+  if gpuDeviceCount() > 0
+    gdev = gpuDevice();
+    maxnumel = gdev.FreeMemory / 8;	% use all gpu memory
+  else
+    maxnumel = 32e9 / 8; 		% use 32GB memory
+  end
+  ns = size(m.svtr, 1);   % number of support vectors
+  nd = size(m.svtr, 2);   % number of dimensions
+  batchsize = floor(maxnumel / (2*ns + nd) - 20);
+  if (batchsize < 1) error('compute_kernel: Out of memory'); end;
+end
