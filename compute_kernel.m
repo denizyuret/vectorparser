651a717a9534682314e2e5ececbd6b6b533ddf21
Deniz Yuret <denizyuret@gmail.com>
2014-08-24 10:19:45 +0300

    Added cache support.

--- compute_kernel.m
+++ compute_kernel.m
@@ -1,3 +1,8 @@
+% TODO: profile
+% TODO: test with perceptron, vectorparser, beamparser,
+% with/without cache, test, train, dump etc.
+% TODO: integrate with perceptron, tparser and retest.
+
 function scores = compute_kernel(m, x)
 
 % compute_kernel.m  (c) Deniz Yuret, 2014
@@ -26,8 +31,9 @@ function scores = compute_kernel(m, x)
 %
 % Implementation notes:
 % - Handle multiple sv blocks outside.  Do it using two models and adding results.
-% - Handle multiple x blocks in here.
+% - Handle multiple x blocks and memory checking in here.
 
+  maxram = 32e9;          % max ram to use (32GB) if no gpu
   nd = size(x, 1);        % dimensionality of instances / support vectors
   nx = size(x, 2);        % number of instances
   nc = size(m.beta, 1);   % number of classes
@@ -43,46 +49,63 @@ function scores = compute_kernel(m, x)
         todo(i) = false;
       end
     end
-    x = x(:,todo);
+    xtodo = x(:,todo);
+    nxtodo = sum(todo);
+  else
+    xtodo = x;
+    nxtodo = nx;
   end
 
-  if size(x, 2) == 0
+  if nxtodo == 0
     % either nothing to do or all was cache hit
     return
   end
 
   hp = m.kerparam;
-  if isfield(m, 'average') && (m.average == 1)
+  if isfield(m, 'average') && m.average
     beta = m.beta2;
   else
     beta = m.beta;
   end
 
-  % DONE: compare with perceptron train/test
-  % DONE: compare with vectorparser train/test
-  % DONE: add the bsxfun trick for poly kernel
-  % DONE: check the bsxfun trick for rbf kernel
-  % DONE: compare with vectorparser with cache
-  % TODO: implement x batching, use it for cache calculation,
-  % perceptron testing etc.
-  % TODO: check the efficiency of todo indexing, especially when nx
-  % is huge.
-
-  if strcmp(hp.type, 'rbf')
-    x2 = sum(x .^ 2, 1);
-    s2 = sum(m.svtr .^ 2, 2);
-    scores(:,todo) = gather(beta * exp(-hp.gamma * bsxfun(@plus, x2, bsxfun(@plus, s2, -2 * (m.svtr * x)))));
-    clear x2 s2;
-  elseif strcmp(hp.type, 'poly')
-    if size(x, 2) == 1
-      % This hack roughly doubles speed on gpu if x is a single vector, sorry:
-      scores(:,todo) = gather(sum(bsxfun(@times, beta', (hp.gamma * (m.svtr * x) + hp.coef0).^hp.degree),1))';
-    else
-      scores(:,todo) = gather(beta * (hp.gamma * (m.svtr * x) + hp.coef0) .^ hp.degree);
-    end
+  % See how much memory we can use
+  if isfield(m, 'gpu') && m.gpu
+    gdev = gpuDevice();
+    maxnumel = gdev.FreeMemory / 8;	% use all gpu memory
   else
-    error('Unknown kernel type %s', hp.type);
-  end  % if strcmp(hp.type, xxx)
+    gdev = [];
+    maxnumel = maxram / 8; 		% use 32GB memory
+  end
+
+  % Figure out the maximum batch size nk
+  nk = floor(maxnumel / (2*ns + nd) - 20);
+  if (nk < 1) error('compute_kernel: Out of memory'); end;
+
+  fxtodo = zeros(nc, nxtodo);
+  for i=1:nk:nxtodo
+    j = min(nxtodo, i+nk-1);
+    xij = xtodo(:,i:j);
+    if ~isempty(gdev)
+      xij = gpuArray(xij);
+    end
+    if strcmp(hp.type, 'rbf')
+      x2 = sum(xij .^ 2, 1);
+      s2 = sum(m.svtr .^ 2, 2);
+      fxtodo(:,i:j) = gather(beta * exp(-hp.gamma * bsxfun(@plus, x2, bsxfun(@plus, s2, -2 * (m.svtr * xij)))));
+      clear x2 s2;
+    elseif strcmp(hp.type, 'poly')
+      if size(xij, 2) == 1
+        % This hack roughly doubles speed on gpu if x is a single vector, sorry:
+        fxtodo(:,i:j) = gather(sum(bsxfun(@times, beta', (hp.gamma * (m.svtr * xij) + hp.coef0).^hp.degree),1))';
+      else
+        fxtodo(:,i:j) = gather(beta * (hp.gamma * (m.svtr * xij) + hp.coef0) .^ hp.degree);
+      end
+    else
+      error('Unknown kernel type %s', hp.type);
+    end  % if strcmp(hp.type, xxx)
+    clear xij;
+  end  % for i=1:nk:nx
+  scores(:,todo) = fxtodo;
 
 end  % compute_kernel
 
