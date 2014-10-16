39ccce8742631d07415061a32fc67bd3f1d08019
Deniz Yuret <dyuret@biyofiz-4-0.local>
2014-10-16 21:24:07 +0300

    Fixed numerical inaccuracy in computing hash keys due to the difference in matmult vs vector product.

--- kernelcache.m
+++ kernelcache.m
@@ -1,64 +1,84 @@
 classdef kernelcache < handle
 
-properties
-size;
-keys;
-vals;
-rvec;
-mean;
-std;
-hit;
-miss;
-nkeys;
-end % properties
+    properties
+        size;
+        keys;
+        vals;
+        rvec;
+        mean;
+        std;
+        hit;
+        miss;
+        nkeys;
 
-methods
+        %%%DBG
+        % keys_orig;
+        % vals_orig;
+        %%%DBG
 
-function c = kernelcache(keys, vals);
-ndims = size(keys, 1);
-nkeys = size(keys, 2);
-c.size = 100 * nkeys;
-c.keys = cell(1, c.size);
-c.vals = cell(1, c.size);
-c.rvec = rand(1, ndims);
-if strcmp(class(keys), 'single')
-    c.rvec = single(c.rvec);
-end
-normval = c.rvec * keys;
-c.mean = mean(normval);
-c.std = std(normval);
-cnum = cellnum(c, keys, normval);
-c.nkeys = 0;
-for i=1:nkeys
-  j = cnum(i);
-  if isempty(c.keys{j}) c.nkeys = c.nkeys + 1; end;
-  c.keys{j} = keys(:,i);
-  c.vals{j} = vals(:,i);
-end
-c.hit = 0;
-c.miss = 0;
-end
+    end % properties
 
-function cnum = cellnum(c, keys, normval)
-if nargin < 3
-  normval = c.rvec * keys;
-end
-unifval = normcdf(normval, c.mean, c.std);
-cnum = ceil(c.size * unifval);
-end
+    methods
 
-function val = get(c, key)
-cnum = cellnum(c, key);
-ckey = c.keys{cnum};
-if (~isempty(ckey) && all(ckey == key))
-  val = c.vals{cnum};
-  c.hit = c.hit + 1;
-else
-  val = [];
-  c.miss = c.miss + 1;
-end
-end
+        function c = kernelcache(keys, vals);
+            ndims = size(keys, 1);
+            nkeys = size(keys, 2);
+            c.size = 100 * nkeys;
+            c.keys = cell(1, c.size);
+            c.vals = cell(1, c.size);
+            rng(1);  % for reproducible random numbers
+            c.rvec = rand(1, ndims);
 
-end % methods
+            %%%DBG
+            % c.keys_orig = keys;
+            % c.vals_orig = vals;
+            %%%DBG
+
+            % This should be replaced with rand(x,y,'like',keys) in
+            % a future matlab version.
+            if strcmp(class(keys), 'single')
+                c.rvec = single(c.rvec);
+            end
+
+            normval = c.rvec * keys;
+            c.mean = mean(normval);
+            c.std = std(normval);
+
+            % This works fast but gives slightly different results
+            % cnum = cellnum(c, keys, normval);
+
+            c.nkeys = 0;
+            for i=1:nkeys
+                % j = cnum(i);
+                j = cellnum(c, keys(:,i));
+                if isempty(c.keys{j}) c.nkeys = c.nkeys + 1; end;
+                c.keys{j} = keys(:,i);
+                c.vals{j} = vals(:,i);
+            end
+            c.hit = 0;
+            c.miss = 0;
+        end
+
+        function cnum = cellnum(c, keys, normval)
+            if nargin < 3
+                normval = c.rvec * keys;
+            end
+            unifval = normcdf(normval, c.mean, c.std);
+            cnum = ceil(c.size * unifval);
+        end
+
+        function val = get(c, key)
+            cnum = cellnum(c, key);
+            ckey = c.keys{cnum};
+            if (~isempty(ckey) && all(ckey == key))
+                val = c.vals{cnum};
+                c.hit = c.hit + 1;
+            else
+                val = [];
+                c.miss = c.miss + 1;
+            end
+        end
+
+    end % methods
 
 end % classdef
